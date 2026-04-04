f74a5a06e3010937333430ed9751613cf6772bea
Jas-n <14942820+Jas-n@users.noreply.github.com>
2026-04-04 21:50:03 +0100

    Remote deploy #44

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -86,7 +86,7 @@ new #[Layout('layouts::auth', ['title' => 'Log In'])] class extends Livewire
 
     <flux:card>
         <flux:heading class="text-center" size="lg">
-            {{ ___('dictionary.login') }} - 14
+            {{ ___('dictionary.login') }} - 15
         </flux:heading>
 
         <x-separator />
